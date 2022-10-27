76d88f8d883281a5cd7140ba74b7632e7d47a0fa
GramxPersons <97328690+GramxPerson@users.noreply.github.com>
2022-10-27 18:15:24 +0700

    BloxFruits Premium

---
+++ phantom-loader.lua
@@ -0,0 +1,8 @@
+if _G.wl_key then
+  return
+end
+local placeId = game.PlaceId
+
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/13174/BloxFruitsPro.lua'))()
+end
