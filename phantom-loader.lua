523391dfe505d172ac5b4597fa4aa38705991a09
GramxPersons <97328690+GramxPerson@users.noreply.github.com>
2022-12-01 18:52:28 +0700

    Delete phantom-loader.lua

--- phantom-loader.lua
+++
@@ -1,5 +0,0 @@
-local placeId = game.PlaceId
-
-if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
-    loadstring(game:HttpGet('https://scripts.luawl.com/13174/BloxFruitsPro.lua'))()
-end
